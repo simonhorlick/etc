9358907147f7f8d9ed2f5150381d200037b828e2
Simon Horlick <simonhorlick@gmail.com>
2024-09-24 23:01:01 +0800

    fix rid

--- cf-dns/patch-record.sh
+++ cf-dns/patch-record.sh
@@ -5,14 +5,11 @@ source /etc/cf-dns.env
 ip=$(ip addr show dev enp9s0f0 | grep inet6 | grep global | grep -v deprecated | awk '{print $2}' | cut -d/ -f1 | head -1)
 
 # The RID is from this request:
-#curl --request GET \
-#  --url https://api.cloudflare.com/client/v4/zones/$ZONEID_JUSI/dns_records \
-#  --header 'Content-Type: application/json' \
-#  --header "Authorization: Bearer $CF_AUTH_TOKEN"
+# curl --request GET --url https://api.cloudflare.com/client/v4/zones/$ZONEID_JUSI/dns_records --header 'Content-Type: application/json' --header "Authorization: Bearer $CF_AUTH_TOKEN"
 
 ZONEID_JUSI=c31459017fe654392d87c13d9a5d44c9
 
-RID=09831fab8d01e7d7b470611d4e38d344
+RID=614693690cd8be5e6585d0a0dd46148a
 curl -vvv --request PUT \
   --url https://api.cloudflare.com/client/v4/zones/$ZONEID_JUSI/dns_records/$RID \
   --header 'Content-Type: application/json' \
